06d768e5febdae6acc8e8fb77c6eb92c4d426996
joochang <slowstarter.gl@gmail.com>
2018-12-20 01:48:54 +0900

    -- Add to previous

--- src/main/java/com/slowstarter/controller/FruitController.java
+++ src/main/java/com/slowstarter/controller/FruitController.java
@@ -5,16 +5,20 @@ import com.slowstarter.properties.FruitProperty2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+//@Controller
+@RestController // ResponseBody가 포함되어 기본 json 으로 출력됨
 public class FruitController {
 
     private FruitProperty2 fruitProperty2;
 
-    @Autowired
+
+//    @Autowired // 특정 Spring 버전 이후로 하나의 DI에 대해서는 어노테이션 붙이지 않아도 됌.
+
     public FruitController(FruitProperty2 fruitProperty2) {
         this.fruitProperty2 = fruitProperty2;
     }
@@ -30,5 +34,11 @@ public class FruitController {
         fruit.setColor("red");
 
         return fruit;
+    }
+
+    @PutMapping("/v1/fruit/{name}")
+    public Boolean setFruitInfo(@PathVariable("name") String name) {
+
+        return Boolean.TRUE;
     }
 }
